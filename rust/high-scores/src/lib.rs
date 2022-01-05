d534a9db65115822ecba1c106106cb34021601e9
pigooosuke <yusuke.saito@binc.jp>
2022-01-06 00:48:26 +0900

    add high scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut scores = self.scores.to_vec();
+        scores.sort_unstable();
+        scores.into_iter().rev().take(3).collect::<Vec<u32>>()
+    }
+}
